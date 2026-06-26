0c303d47c8d107c26e4b2ef8e1e8859e64922ddc
ShahHasan05 <shahhasanhanif@gmail.com>
2026-06-26 21:45:28 +0530

    C# files

---
+++ repos/learningApp/learningApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace learningApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new StringBuilder();
+            for (int i = 0; i <= 5; i++)
+            {
+                builder.Append("item ").Append(i).Append(",");
+            }
+            var result = builder.ToString();
+            Console.WriteLine(result);
+            string name = "Hasan";
+            if (name is not null)
+            {
+                Console.WriteLine($"Name has {name.Length} characters.");
+            }
+            else
+            {
+                Console.WriteLine("Name is null.");
+            }
+            string a = "Hello";
+            String b = "Hello";
+            Console.WriteLine(a == b);
+            Console.WriteLine(typeof(string) == typeof(String));
+        }
+    }
+}
