a5c1b4a6fc42d8d0b3940ac47f7c7a1c4a1395a9
HasanShah05 <shahhasanhanif@gmail.com>
2026-06-29 15:40:53 +0530

    first commit

---
+++ learningApp/learningApp/StorageExmaple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace memoryTest
+{
+    public struct Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            Person p1 = new("Hasan", 21);
+            Console.WriteLine($"p1 Name: {p1.Name}, Age: {p1.Age}");
+
+            Person p2 = p1;
+            p2.Name = "Hanna";
+            p2.Age = 25;
+            Console.WriteLine($"p2 Name: {p2.Name}, Ahe: {p2.Age}");
+            Console.WriteLine($"p1 Name: {p1.Name}, Age: {p1.Age}");
+        }
+    }
+}
