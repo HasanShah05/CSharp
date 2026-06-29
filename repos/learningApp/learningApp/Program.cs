413f7c84c6028bc29e09e5f9cd2d7963f486d41f
ShahHasan05 <shahhasanhanif@gmail.com>
2026-06-29 12:08:44 +0530

    added new project

--- repos/learningApp/learningApp/Program.cs
+++ repos/learningApp/learningApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text;
+using System;
+using Npgsql;
 
 namespace learningApp
 {
@@ -6,6 +8,32 @@ namespace learningApp
     {
         static void Main(string[] args)
         {
+            string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=admin;Database=testdb";
+
+            using var connection = new NpgsqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected to postgressql succesfully");
+
+                string query = "Select * from students";
+
+                using var command = new NpgsqlCommand(query, connection);
+                using var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string name = reader.GetString(0);
+                    int rollNo = reader.GetInt32(1);
+
+                    Console.WriteLine($"{name} - {rollNo}");
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("oh no there is an error");
+            }
 
             var builder = new StringBuilder();
             for (int i = 0; i <= 5; i++)
@@ -14,10 +42,10 @@ namespace learningApp
             }
             var result = builder.ToString();
             Console.WriteLine(result);
-            string name = "Hasan";
-            if (name is not null)
+            string name2 = "Hasan";
+            if (name2 is not null)
             {
-                Console.WriteLine($"Name has {name.Length} characters.");
+                Console.WriteLine($"Name has {name2.Length} characters.");
             }
             else
             {
--- repos/learningApp/learningApp/obj/Debug/net10.0/learningApp.AssemblyInfo.cs
+++ repos/learningApp/learningApp/obj/Debug/net10.0/learningApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c303d47c8d107c26e4b2ef8e1e8859e64922ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
